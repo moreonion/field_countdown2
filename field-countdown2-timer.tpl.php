0ba10f41d8be24961eb0190a61a0ddc65efc00b0
Michael Strasser <mst1409@gmx.at>
2017-08-29 17:47:14 +0200

    Officially responsive

--- field-countdown2-timer.tpl.php
+++ field-countdown2-timer.tpl.php
@@ -8,7 +8,7 @@
 
 <div class='countdown-timer'>
   <div id='<?php print $timer_id ?>'
-   class='countdown-timer-countdown'>
+   class='countdown-timer-countdown clock'>
   </div>
   <?php if($text_timer): ?>
     <div id='<?php print $note_id; ?>'
--- field_countdown2.js
+++ field_countdown2.js
@@ -11,33 +11,14 @@
       var i = 0;
       $.each($('.countdown-timer', context), function (index, element) {
         var countdown = $('.countdown-timer-countdown', $(this));
-        var note = $('.countdown-timer-note', $(this));
         var settings = Drupal.settings.field_countdown2[countdown.attr('id')];
-        var field_time = settings['countdown-settings-time'];
-
-        ts = new Date(field_time * 1000);
-        countdown.countdown({
-          timestamp: ts,
-          callback: function (days, hours, minutes, seconds) {
-            var date_time_str = new Array();
-            date_time_str['@days'] = Drupal.formatPlural(
-                    days, '1 day', '@count days'
-                    );
-            date_time_str['@hours'] = Drupal.formatPlural(
-                    hours, '1 hour', '@count hours'
-                    );
-            date_time_str['@minutes'] = Drupal.formatPlural(
-                    minutes, '1 minute', '@count minutes'
-                    );
-            date_time_str['@seconds'] = Drupal.formatPlural(
-                    seconds, '1 second', '@count seconds'
-                    );
-            var message = Drupal.t(
-                    '@days, @hours, @minutes and @seconds left',
-                    date_time_str
-                    );
-            note.html(message);
-          }
+        var ts = new Date(settings['countdown-settings-time'] * 1000);
+        console.log(ts);
+        var left = Math.floor((ts - (new Date())) / 1000);
+        console.log(left);
+        var clock = $('.countdown-timer-countdown', $(this)).FlipClock( left, {
+          clockFace: 'DailyCounter',
+          countdown: true
         });
       });
     }
