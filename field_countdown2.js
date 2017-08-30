707c3ec3bac7670ea08ba866790918a9935cc5ec
Michael Strasser <mst1409@gmx.at>
2017-08-30 11:14:04 +0200

    Timer labels displayed according to node language

--- field_countdown2.js
+++ field_countdown2.js
@@ -13,12 +13,11 @@
         var countdown = $('.countdown-timer-countdown', $(this));
         var settings = Drupal.settings.field_countdown2[countdown.attr('id')];
         var ts = new Date(settings['countdown-settings-time'] * 1000);
-        console.log(ts);
         var left = Math.floor((ts - (new Date())) / 1000);
-        console.log(left);
         var clock = $('.countdown-timer-countdown', $(this)).FlipClock( left, {
           clockFace: 'DailyCounter',
-          countdown: true
+          countdown: true,
+          language: settings['countdown-settings-language'],
         });
       });
     }
