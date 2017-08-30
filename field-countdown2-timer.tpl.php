c7c5ed5a17a188501354b73e7517e49762e4abe6
Michael Strasser <mst1409@gmx.at>
2017-08-30 13:10:37 +0200

    Added heading text field

--- field-countdown2-timer.tpl.php
+++ field-countdown2-timer.tpl.php
@@ -7,6 +7,9 @@
 ?>
 
 <div class='countdown-timer'>
+  <?php if ($heading): ?>
+    <h3><?php print $heading; ?></h3>
+  <?php endif; ?>
   <div id='<?php print $timer_id ?>'
    class='countdown-timer-countdown clock'>
   </div>
