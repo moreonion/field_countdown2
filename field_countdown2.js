e805be48ea4f31e81b8a26c8d37ef34fa26c7463
Michael Strasser <mst1409@gmx.at>
2017-08-30 11:19:49 +0200

    Negative remaining time sets countdown to zero

--- field_countdown2.js
+++ field_countdown2.js
@@ -14,6 +14,9 @@
         var settings = Drupal.settings.field_countdown2[countdown.attr('id')];
         var ts = new Date(settings['countdown-settings-time'] * 1000);
         var left = Math.floor((ts - (new Date())) / 1000);
+        if (left < 0) {
+          left = 0;
+        }
         var clock = $('.countdown-timer-countdown', $(this)).FlipClock( left, {
           clockFace: 'DailyCounter',
           countdown: true,
